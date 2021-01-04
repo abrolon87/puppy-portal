415569478c5192d16c3a5bfa7220d8678c78acbd
Amanda Rolon <abrolon87@gmail.com>
2021-01-04 15:02:51 -0500

    adds sign up and login buttons; adds routes for sign up and login

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   root "sessions#home"
+  
+  get '/signup' => 'users#new'
+  post '/signup' => 'users#create'
+
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
 
   resources :cues
   resources :puppies
