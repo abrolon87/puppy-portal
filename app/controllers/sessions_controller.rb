3096d7f1b6f9963aa31b55326d9807325ffe13fc
Amanda Rolon <abrolon87@gmail.com>
2021-01-04 14:16:51 -0500

    generates sessions_controller

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,7 @@
+class SessionsController < ApplicationController
+  def home 
+  end
+
+  def login 
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root "sessions#home"
+
   resources :cues
   resources :puppies
   resources :users
