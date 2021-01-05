299980cf0e633232c09061c242d9b63bbd7d598b
Amanda Rolon <abrolon87@gmail.com>
2021-01-05 14:18:13 -0500

    adds validations; successfully logs a user in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,4 +4,17 @@ class SessionsController < ApplicationController
 
   def login 
   end
+
+  def create 
+    user = User.find_by(username: params[:user][:username])
+    
+    if user && user.authenticate(params[:user][:password])
+      session[:user_id] = user.id
+      redirect_to user_path(user) # '/users/#{user.id}'
+    else  
+      flash[:message] = "Invalid Login Info, Please Try Again"
+      redirect_to '/login'
+    end
+  end
+
 end
