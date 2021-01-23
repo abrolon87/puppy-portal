ef840689a602b1204ad70f4403ee61134ae67efd
Amanda Rolon <abrolon87@gmail.com>
2021-01-23 12:51:10 -0500

    adds current_user helper method where needed; successfully able to create a puppy that belings to current_user

--- app/controllers/puppies_controller.rb
+++ app/controllers/puppies_controller.rb
@@ -1,7 +1,32 @@
 class PuppiesController < ApplicationController
 
+  def index 
+    @puppies = current_user.puppies.all
+  end 
+
   def new 
     @puppy = Puppy.new 
-    
+    @user = @puppy.build_user
   end
+
+  def create 
+    binding.pry
+    @puppy = current_user.puppies.create(puppy_params)
+    if @puppy.save 
+      redirect_to puppy_path(@puppy)
+    else  
+      binding.pry 
+    end
+  end
+
+  def show 
+    find_puppy
+  end
+
+  private 
+
+  def puppy_params 
+    params.require(:puppy).permit(:name, :breed, :location, :bio, :user_id)
+  end
+
 end
