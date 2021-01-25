4b9962fae6071e7655a113b40bd72fd71f6e16ca
Amanda Rolon <abrolon87@gmail.com>
2021-01-24 20:28:42 -0500

    adds dependent: delete_all

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
-  has_many :puppies
+  has_many :puppies, dependent: :delete_all
+
   validates  :username, presence: true, uniqueness: true
   validates :email, uniqueness: true
 end
